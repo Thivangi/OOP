e89d7328d0dde38cdc27d05c82c4e203aecb07bc
Thivangi <thivangikumari@gmail.com>
2019-10-21 13:13:07 +0530

    Add files via upload

---
+++ interfaces/src/interfaces/Speaker.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author AsusVivo
+ */
+public interface Speaker {
+    
+    //if you have constant we can add them here
+    //or else we can add the method
+    //answers the question for what
+    //doent answer how
+    //public static final count=5;
+    public void speak();
+    
+}
